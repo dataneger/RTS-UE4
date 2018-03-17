74a84fd8bbee9bd6c7412135e933a1b7b920185c
dataneger <herodias@gmail.com>
2018-03-17 22:51:17 +0100

    initial project c++

---
+++ Source/RTSUE4/RTSUE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RTSUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RTSUE4, "RTSUE4" );
